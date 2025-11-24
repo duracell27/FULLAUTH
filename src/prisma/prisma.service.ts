960bb51e84c74e9741d00ed6f85c4ea8235bdb19
Duracell27 <volodymyrshmidt@gmail.com>
2025-11-24 12:58:13 +0200

    fix one more time prisma 7 ver

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { Pool } from 'pg'
 
 @Injectable()
 export class PrismaService
@@ -7,7 +9,10 @@ export class PrismaService
 	implements OnModuleInit, OnModuleDestroy
 {
 	constructor() {
-		super()
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment
+		const pool = new Pool({ connectionString: process.env.POSTGRES_URL })
+		const adapter = new PrismaPg(pool)
+		super({ adapter })
 	}
 
 	public async onModuleInit(): Promise<void> {
